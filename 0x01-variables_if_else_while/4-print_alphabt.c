2635ef19e7da5a96f3b195abd531726a950ddcf6
thusadumi19 <dumisanithusabantu@gmail.com>
2022-12-09 16:26:53 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,12 +10,12 @@ int main(void)
 
 	while (c <= 'z')
 	{
-		if (c != 'e' && c!= 'z')
+		if (c != 'e' && c != 'q')
 		{
 			putchar(c);
 		}
 		c++;
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
