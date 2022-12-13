32a004c6a758f6107b527dba8d6afd63453090a7
thusadumi19 <dumisanithusabantu@gmail.com>
2022-12-13 15:59:25 -0800

    task-1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints the alpha in lowercase
+ *
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+	_putchar('\n');
+}
