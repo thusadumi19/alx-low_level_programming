c0cbfc69de9bb5f66f2643f86be14d44ba9110d3
thusadumi19 <dumisanithusabantu@gmail.com>
2022-12-19 16:23:17 -0800

    task4

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,11 +4,14 @@
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
  */
+
 void _puts(char *str)
 {
-	while (*str != '\0')
+	int index;
+
+	for (index = 0; str[index] != '\0'; index++)
 	{
-		_putchar(*str++);
+		_putchar(str[index]);
 	}
 	_putchar('\n')
 
