4565b5fb96fe62f76c48d7120e2187970b7a36ff
thusadumi19 <dumisanithusabantu@gmail.com>
2022-12-19 16:07:09 -0800

    task5

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,14 +3,12 @@
 /**
  * print_rev - imprime en reversa
  * @s: string
- * Return: 0
  */
 void print_rev(char *s)
 {
 	int i = 0;
 
 	while (s[i] != '\0')
-
 		i++;
 	for (i = i - 1; i >= 0; 1--)
 		_putchar(s[i]);
