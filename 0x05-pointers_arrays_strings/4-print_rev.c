e4207ea380be8031e974d1c17f337bd16a7368be
thusadumi19 <dumisanithusabantu@gmail.com>
2022-12-19 16:02:15 -0800

    task5

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,19 +7,14 @@
  */
 void print_rev(char *s)
 {
-	int longi = 0;
-	int o;
+	int i = 0;
 
-	while (*s != '\0')
-	{
-		longi++;
-		s++;
-	}
-	s--;
-	for (o = longi; o > 0; o--)
-	{
-		_putchar(*s);
-	}
+	while (s[i] != '\0')
+
+		i++;
+	for (i = i - 1; i >= 0; 1--)
+		_putchar(s[i]);
+
 	_putchar('\n');
 
 }
