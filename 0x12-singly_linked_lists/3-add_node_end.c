b2edb7bf3c0095e49d801d25e236e84f547ae6b9
thusadumi19 <dumisanithusabantu@gmail.com>
2023-01-26 02:59:11 -0800

    task3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -5,13 +5,16 @@
 /**
  * _strlen - finds the length of a string
  * @str: string to find the length of
+ *
  * Return: length of string
  */
+
 unsigned int _strlen(char *str)
 {
 	unsigned int i;
 
 	for (i = 0; str[i]; i++)
+
 	return (i);
 }
 
@@ -21,15 +24,16 @@ unsigned int _strlen(char *str)
  * @str: string to add to the new node
  * Return: pointer to the new nod
  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new, *tmp;
 
 	if (str == NULL)
-	return (NULL);
+		return (NULL);
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
-	return (NULL);
+		return (NULL);
 	new->str = strdup(str);
 	if (new->str == NULL)
 	{
